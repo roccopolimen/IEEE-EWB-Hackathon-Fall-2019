1042c5fde96bcba0350d5d92efae174a1ca7d09d
Rocco Polimeni <rpolimen@stevens.edu>
2019-10-19 13:45:26 -0400

    remove dindex.html

--- webapp/app.js
+++ webapp/app.js
@@ -8,26 +8,16 @@ var state = "";
 var yield = "";
 var profit = "";
 
-var submit = document.getElementById("submit");
 
-submit.onclick = function(){
-    var crop = document.getElementById("crop").value;
-    var city = document.getElementById("city").value;
-    var state = document.getElementById("state").value;
-    var yield = document.getElementById("yield").value;
-    var profit = document.getElementById("profit").value;
-
-
-    CSVToJSON().fromFile("./dataset.csv").then(source => {
-        console.log(source);
-        source.push({
-            "crop": crop,
-            "city": city,
-            "state": state,
-            "yield": yield,
-            "profit": profit
-        });
-        const csv = JSONToCSV(source, {fields: ["crop", "city", "state", "yield", "profit"]});
-        FileSystem.writeFileSync("./dataset.csv", csv);
+CSVToJSON().fromFile("./dataset.csv").then(source => {
+    console.log(source);
+    source.push({
+        "crop": crop,
+        "city": city,
+        "state": state,
+        "yield": yield,
+        "profit": profit
     });
-}
+    const csv = JSONToCSV(source, {fields: ["crop", "city", "state", "yield", "profit"]});
+    FileSystem.writeFileSync("./dataset.csv", csv);
+});
---
+++ webapp/form.js
@@ -0,0 +1,10 @@
+var submit = document.getElementById("submit");
+
+submit.onclick = function(){
+    var crop = document.getElementById("crop").value;
+    var city = document.getElementById("city").value;
+    var state = document.getElementById("state").value;
+    var yield = document.getElementById("yield").value;
+    var profit = document.getElementById("profit").value;
+
+}
