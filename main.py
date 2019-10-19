b1ab2ac81f2f689fe6cea77e507e05fdcce08e31
Rocco Polimeni <rpolimen@stevens.edu>
2019-10-19 11:01:06 -0400

    remove unnecessary files

--- main.py
+++
@@ -1,3 +0,0 @@
-print('hello world')
-print('Tristan waz here')
-print('Marco is my sweetheart. I love you <3')
--- tristan.py
+++
@@ -1,2 +0,0 @@
-print('So you stumbbled on my little .py file')
-print('Welcome traveler, join me in the splender of my people')
