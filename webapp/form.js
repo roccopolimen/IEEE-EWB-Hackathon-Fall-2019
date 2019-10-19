e76067293cf7523b044f94271975022acd205a77
Tristan Kensinger <tristan.kensinger@gmail.com>
2019-10-19 12:15:42 -0400

    updates

---
+++ webapp/form.js
@@ -0,0 +1,11 @@
+var submit = document.getElementById("submit");
+
+var location="hi", name, yield;
+
+submit.onclick = function(){
+    location = document.getElementById("location");
+    name = document.getElementById("name");
+    yield = document.getElementById("yield");
+}
+
+console.log(location);
