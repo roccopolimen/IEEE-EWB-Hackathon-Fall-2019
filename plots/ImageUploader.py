c5287c938792d65d46034e4e9676952c4dbf123c
Marco Polimeni <stats3434@gmail.com>
2019-10-19 13:37:36 -0400

    test commit

---
+++ plots/ImageUploader.py
@@ -0,0 +1,7 @@
+from git import Repo
+
+repo = Repo("D:\Stevens Institute of Technology\IEEE\IEEE-EWB-Hackathon-Fall-2019")
+print(repo.untracked_files)
+repo.index.add(["D:\Stevens Institute of Technology\IEEE\IEEE-EWB-Hackathon-Fall-2019\plots"])
+print(repo.untracked_files)
+repo.git.commit('-m', 'test commit', author='stats3434@gmail.com')
