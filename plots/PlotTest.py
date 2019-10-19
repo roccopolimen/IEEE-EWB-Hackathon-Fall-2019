2f3db858ece9850824902cc95c9c610ee6a89a8b
Marco Polimeni <stats3434@gmail.com>
2019-10-19 15:38:14 -0400

    Fill crops

--- plots/PlotTest.py
+++ plots/PlotTest.py
@@ -4,13 +4,36 @@ import matplotlib.pyplot as plt
 
 
 # Import nyc filepath for pumpkin prices
-new_york_filepath = "a-year-of-pumpkin-prices/new-york_9-24-2016_9-30-2017.csv"
-new_york = pd.read_csv(new_york_filepath)
+# new_york_filepath = "a-year-of-pumpkin-prices/new-york_9-24-2016_9-30-2017.csv"
+# new_york = pd.read_csv(new_york_filepath)
 
 # See the first couple of values in the nyc csv file
-new_york.head()
-print(new_york.head())
+# new_york.head()
+# print(new_york.head())
 
-swarm_plot = sns.swarmplot(x=new_york['Variety'],y=new_york['High Price'])
-fig = swarm_plot.get_figure()
-fig.savefig("pricentype.png")
+# swarm_plot = sns.swarmplot(x=new_york['Variety'],y=new_york['High Price'])
+# fig = swarm_plot.get_figure()
+# fig.savefig("pricentype.png")
+
+
+
+
+
+filepath = "..\webapp\dataset.csv"
+data = pd.read_csv(filepath)
+
+# print(data.head())
+# plot = sns.swarmplot(x=data[''], y=data[''])
+# fig = plot.get_figure()
+# fig.savefig("price_over_yield")
+
+def fillCrops(data):
+	cropNames = data.crop.unique()
+	crops = {}
+	# print(allCrops)
+	for crop in cropNames:
+		crops[crop] = data[data.crop == crop]
+
+	return crops
+
+fillCrops(data)
