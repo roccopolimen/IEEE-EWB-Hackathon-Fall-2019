3cc4a1eccdcd7527dd9fabd2382486b58d39375e
Tristan Kensinger <tristan.kensinger@gmail.com>
2019-10-19 16:24:04 -0400

    slowly dieing

---
+++ webapp/text.js
@@ -0,0 +1,6 @@
+
+    const crop = document.getElementById("crop").value;
+    const city = document.getElementById("city").value;
+    const state = document.getElementById("state").value;
+    const yields = document.getElementById("yield").value;
+    const profit = document.getElementById("profit").value;
