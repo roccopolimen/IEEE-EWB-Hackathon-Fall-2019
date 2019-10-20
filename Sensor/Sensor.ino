fab205cd4561ce2efb07b22738821fc6ee1d8381
Quentin <qjimenez@stevens.edu>
2019-10-20 17:47:36 -0400

    DataToCSV

--- Sensor/Sensor.ino
+++ Sensor/Sensor.ino
@@ -19,6 +19,13 @@ void loop()
   sensorVals[0] = (int)DHT11.humidity;
   sensorVals[1] = (int)DHT11.temperature;
   sensorVals[2] = analogRead(0);
+
+  if(sensorVals[2] < 200)
+  {
+    sensorVals[2] = 0;
+  }else{
+    sensorVals[2] = (sensorVals[2]-200)/3;
+  }
   
 
   Serial.print(sensorVals[0]);
@@ -26,7 +33,7 @@ void loop()
   Serial.print(sensorVals[1]);
   Serial.print(",");
   Serial.print(sensorVals[2]);
-  Serial.println("\n");
+  Serial.println("");
   delay(2000);
 
 }
